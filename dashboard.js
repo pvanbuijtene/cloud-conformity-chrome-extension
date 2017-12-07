1b668ab274d3718ec8dc924983277d572d565a5a
Pascal van Buijtene <pvanbuijtene@users.noreply.github.com>
2017-12-07 17:55:51 +0100

    Show all modules

--- dashboard.js
+++ dashboard.js
@@ -3,6 +3,26 @@
 
     console.log("Setting up Cloud Conformity Dashboard...");
 
+    var moduleIndex = 1; // Start with scrolling to second module.
+
+    function showNextModule() {
+        console.log("Show next module...");
+
+        var navigationBarHeight = document.querySelector(".navigation-bar").clientHeight;
+        var modules = document.querySelectorAll(".module");
+
+        if (moduleIndex < modules.length) {
+            window.scroll({ top: (modules[moduleIndex].offsetTop + navigationBarHeight), left: 0, behavior: 'smooth' });
+
+            moduleIndex++;
+            scheduleShowNextModule();
+        } else {
+            moduleIndex = 1;
+            scheduleShowNextAccount();
+        }
+    }
+
+
     function selectNexAccount() {
         console.log("Switching to next account...");
 
@@ -13,12 +33,24 @@
                 if (i+1 < accounts.length) {
                     next = accounts[i+1];
                 }
+                
+                window.scroll({ top: 0, left: 0, behavior: 'smooth' });
                 next.querySelector("a").click();
             }
         }
+
+        scheduleShowNextModule();
     }
 
-    setInterval(selectNexAccount, 1000 * 30);
+    function scheduleShowNextAccount() {
+        setTimeout(selectNexAccount, 1000 * 30);
+    }
+
+    function scheduleShowNextModule() {
+        setTimeout(showNextModule, 1000 * 30);
+    }
 
     document.styleSheets[0].insertRule(".account-search, .add, #intercom-container { display: none!important; }", 0);
+
+    scheduleShowNextModule();
 })();
